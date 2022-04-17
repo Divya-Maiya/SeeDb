9acfd1efe26cb8e6a2a7bb460e3388bc73bddc6b
Divya-Maiya <91624357+Divya-Maiya@users.noreply.github.com>
2022-04-17 18:27:27 -0400

    Add sql util

---
+++ prepare_data.sql
@@ -0,0 +1,21 @@
+drop table IF EXISTS Census;
+
+create table Census(
+	age INTEGER,
+	workclass CHAR(50),
+	fnlwgt INTEGER,
+	education CHAR(50),
+	education_num INTEGER,
+	marital_status CHAR(50),
+	occupation CHAR(50),
+	relationship CHAR(50),
+	race CHAR(50),
+	sex CHAR(20),
+	capital_gain INTEGER,
+	capital_loss INTEGER,
+	hours_per_week INTEGER,
+	native_country CHAR(50),
+	salary CHAR(50)
+);
+
+\copy Census FROM adult_data.txt with (DELIMITER(','));
