64d72aae5acd5c3b09288bd530134e184a63e812
divyamaiya <dmaiya@umass.edu>
2022-04-19 14:30:17 -0400

    Introduce pruning

---
+++ src/pruning.py
@@ -0,0 +1,11 @@
+def confidence_interval_pruning(aggregate_views, k, ):
+
+    return []
+
+
+def execute_pruning_optimization(aggregate_views, target_cols, reference_cols, k):
+    return []
+
+
+def multi_armed_bandit_pruning():
+    return []
