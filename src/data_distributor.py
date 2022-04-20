a90a2900d65d14790f0c67f51d3725021d301c3c
divyamaiya <dmaiya@umass.edu>
2022-04-20 00:37:08 -0400

    Update splitting

--- src/data_distributor.py
+++ src/data_distributor.py
@@ -5,17 +5,19 @@ import pandas as pd
 import os
 import numpy as np
 
+
 def split_data(splits):
     all_rows = pd.read_csv("../data/adult.data", sep=",")
-    
-    df_split= np.array_split(all_rows,int(splits))
-    for i in range(1,len(df_split)+1):
-        df_split[i-1].to_csv("../data/test_split_{}.csv".format(i),encoding='utf-8', index=False)
+
+    df_split = np.array_split(all_rows, int(splits))
+    for i in range(1, len(df_split) + 1):
+        df_split[i - 1].to_csv("../data/test_split_{}.csv".format(i), encoding='utf-8', index=False)
+
 
 def is_dir_empty(path):
     initial_count = 0
     if os.path.exists(path) and not os.path.isfile(path):
-  
+
         if not os.listdir(path):
             return True
         else:
@@ -26,11 +28,13 @@ def is_dir_empty(path):
     else:
         return True
 
+
 def generate_split_views(cursor, connection, splits):
-    for i in range(1, int(splits)+1):
+    for i in range(1, int(splits) + 1):
         cursor.execute("""
-                       create temp table split_view{} (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, economic_indicator text);
-                       """.format(i, i, i))
+                       drop table if exists split_view{};
+                       create table split_view{} (age real, workclass text, fnlwgt real, education text, education_num real, marital_status text, occupation text, relationship text, race text, sex text, capital_gain real, capital_loss real, hours_per_week real, native_country text, salary text);
+                       """.format(i, i, i, i))
         connection.commit()
         f = open('../data/test_split_{}.csv'.format(i), 'r')
         cursor.copy_from(f, 'split_view{}'.format(i), sep=',')
@@ -41,19 +45,16 @@ def generate_split_views(cursor, connection, splits):
                        drop table if exists split_unmarried_{};
                        create table split_married_{} as (select * from split_view{} where marital_status in (' Married-AF-spouse', ' Married-civ-spouse', ' Married-spouse-absent',' Separated'));
                        create table split_unmarried_{} as (select * from split_view{} where marital_status in (' Never-married', ' Widowed',' Divorced'));
-                       """.format(i,i,i,i,i,i))
+                       """.format(i, i, i, i, i, i))
         connection.commit()
-        
-    
+
 
 def split_data_by_marital_status(cursor, connection):
-    
     cursor.execute(open("../db_scripts/create_main_views.sql", "r").read())
     connection.commit()
 
     cursor.execute("select count(*) from married;")
     print("Married rows = " + str(cursor.fetchone()))
-    
+
     cursor.execute("select count(*) from unmarried;")
     print("Unmarried rows = " + str(cursor.fetchone()))
-    
