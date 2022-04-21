f0cdaca681c587e5f421ae63173e793e29f4246c
Chirag Uday Kamath <cukamath@umass.edu>
2022-04-21 12:28:50 -0400

    Added test data to adult.data after cleanup

---
+++ src/file_cleanup.py
@@ -0,0 +1,13 @@
+c=0
+with open("../data/adult.test") as f:
+    for line in f:
+        c+=1
+        file_object = open('../data/adult_test_clean.test', 'a')
+        if(c==16281):
+            file_object.write(line[:-2]+'K'+ "\n")
+        else:
+            file_object.write(line[:-2]+ "\n")
+        
+        file_object.close()
+        
+print(c)
